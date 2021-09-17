3e651a5986194d5aed5d422f01d1b3cc9b136833
Daniel Batistão <danielbatistaodp2@gmail.com>
2021-09-16 22:10:56 -0300

    Adicionando alterações ao pingmult.py

--- tools/pingmult.py
+++ tools/pingmult.py
@@ -1,7 +1,18 @@
 import os
+import time 
 
-print("#"*30)
+print("#"*60)
 print("Ferramenta de ping multiplo")
-print("#"*30)
+print("#"*60)
 
-ip_or_host = input("Digite o IP ou Host a ser verificado: ")
+with open("hosts.txt") as file:
+	dump = file.read()
+	dump = dump.splitlines()
+	for ip in dump:
+		print()
+		print("-"*60)
+		print(" \"Pingando\" em {}".format(ip))
+		os.system('ping {}'.format(ip))
+		print()
+		print("Ping em {} foi finalizado!".format(ip))
+		time.sleep(5)
